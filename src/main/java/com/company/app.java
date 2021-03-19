a0b2b788299485a305f8242382d9c5c178323acf
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-19 14:32:02 +0300

    start spring-boot

---
+++ src/main/java/com/company/app.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class app {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
