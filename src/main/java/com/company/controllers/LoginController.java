d238ace27472af75295dd05bebffba1bb9166bbc
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-20 01:16:08 +0300

    check on login and password

--- src/main/java/com/company/controllers/LoginController.java
+++ src/main/java/com/company/controllers/LoginController.java
@@ -1,12 +1,18 @@
 package com.company.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String getLoginPage() {
+    public String getLoginPage(HttpServletRequest request, ModelMap model) {
+        if (request.getParameterMap().containsKey("error")) {
+            model.addAttribute("error", true);
+        }
         return "login";
     }
 }
